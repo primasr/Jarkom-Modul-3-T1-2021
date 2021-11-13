52b329be00615639f155c966ce5965dba0a82683
Prima Secondary Ramadhan <prima.s.r.2001@gmail.com>
2021-11-13 21:33:56 +0700

    Create 0.sh

---
+++ Scripts/Tottoland/0.sh
@@ -0,0 +1,7 @@
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+apt-get install nano
+apt-get install dnsutils -y
+apt-get install lynx -y
+apt-get install ca-certificates openssl -y
+apt-get install speedtest-cli -y
