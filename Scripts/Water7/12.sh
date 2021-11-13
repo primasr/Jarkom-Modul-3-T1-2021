dcac3e96a69167de867ebba2af4c4354214b1989
Prima Secondary Ramadhan <73151849+primasr@users.noreply.github.com>
2021-11-13 21:30:47 +0700

    Update 12.sh

--- Scripts/Water7/12.sh
+++ Scripts/Water7/12.sh
@@ -15,3 +15,24 @@ delay_class 2 1
 delay_access 2 allow ZORO
 delay_parameters 2 none
 ' > /etc/squid/acl-bandwidth.conf
+
+echo '
+include /etc/squid/acl.conf
+include /etc/squid/acl-bandwidth.conf
+http_port 5000
+visible_hostname Water7
+
+auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
+auth_param basic children 5
+auth_param basic realm Proxy
+auth_param basic credentialsttl 2 hours
+auth_param basic casesensitive on
+acl USERS proxy_auth REQUIRED
+http_access allow USERS AVAILABLE_WORKING
+
+dns_nameservers 10.42.2.2
+
+http_access deny all
+' > /etc/squid/squid.conf
+
+service squid restart
